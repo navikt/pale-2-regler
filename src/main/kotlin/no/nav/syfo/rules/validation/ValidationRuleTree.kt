f9647d071f0ae6cbf549e849cd87406aa0eea012
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-03-09 15:28:14 +0100

    Fixed compile error

--- src/main/kotlin/no/nav/syfo/rules/validation/ValidationRuleTree.kt
+++ src/main/kotlin/no/nav/syfo/rules/validation/ValidationRuleTree.kt
@@ -9,29 +9,17 @@ import no.nav.syfo.rules.dsl.tree
 
 enum class ValidationRules {
     PASIENT_YNGRE_ENN_13,
-    UGYLDIG_REGELSETTVERSJON,
-    MANGLENDE_DYNAMISKE_SPOERSMAL_VERSJON2_UKE_39,
     UGYLDIG_ORGNR_LENGDE,
     AVSENDER_FNR_ER_SAMME_SOM_PASIENT_FNR,
-    BEHANDLER_FNR_ER_SAMME_SOM_PASIENT_FNR
 }
 
 val validationRuleTree = tree<ValidationRules, RuleResult>(ValidationRules.PASIENT_YNGRE_ENN_13) {
     yes(INVALID, ValidationRuleHit.PASIENT_YNGRE_ENN_13)
-    no(ValidationRules.UGYLDIG_REGELSETTVERSJON) {
-        yes(INVALID, ValidationRuleHit.UGYLDIG_REGELSETTVERSJON)
-        no(ValidationRules.MANGLENDE_DYNAMISKE_SPOERSMAL_VERSJON2_UKE_39) {
-            yes(INVALID, ValidationRuleHit.MANGLENDE_DYNAMISKE_SPOERSMAL_VERSJON2_UKE_39)
-            no(ValidationRules.UGYLDIG_ORGNR_LENGDE) {
-                yes(INVALID, ValidationRuleHit.UGYLDIG_ORGNR_LENGDE)
-                no(ValidationRules.AVSENDER_FNR_ER_SAMME_SOM_PASIENT_FNR) {
-                    yes(INVALID, ValidationRuleHit.AVSENDER_FNR_ER_SAMME_SOM_PASIENT_FNR)
-                    no(ValidationRules.BEHANDLER_FNR_ER_SAMME_SOM_PASIENT_FNR) {
-                        yes(INVALID, ValidationRuleHit.BEHANDLER_FNR_ER_SAMME_SOM_PASIENT_FNR)
-                        no(OK)
-                    }
-                }
-            }
+    no(ValidationRules.UGYLDIG_ORGNR_LENGDE) {
+        yes(INVALID, ValidationRuleHit.UGYLDIG_ORGNR_LENGDE)
+        no(ValidationRules.AVSENDER_FNR_ER_SAMME_SOM_PASIENT_FNR) {
+            yes(INVALID, ValidationRuleHit.AVSENDER_FNR_ER_SAMME_SOM_PASIENT_FNR)
+            no(OK)
         }
     }
 }
@@ -47,10 +35,7 @@ internal fun RuleNode<ValidationRules, RuleResult>.no(status: Status, ruleHit: V
 fun getRule(rules: ValidationRules): Rule<ValidationRules> {
     return when (rules) {
         ValidationRules.PASIENT_YNGRE_ENN_13 -> pasientUnder13Aar
-        ValidationRules.UGYLDIG_REGELSETTVERSJON -> ugyldigRegelsettversjon
-        ValidationRules.MANGLENDE_DYNAMISKE_SPOERSMAL_VERSJON2_UKE_39 -> manglendeDynamiskesporsmaalversjon2uke39
         ValidationRules.UGYLDIG_ORGNR_LENGDE -> ugyldingOrgNummerLengde
         ValidationRules.AVSENDER_FNR_ER_SAMME_SOM_PASIENT_FNR -> avsenderSammeSomPasient
-        ValidationRules.BEHANDLER_FNR_ER_SAMME_SOM_PASIENT_FNR -> behandlerSammeSomPasient
     }
 }
