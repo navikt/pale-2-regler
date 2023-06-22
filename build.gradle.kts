7c7accb7e3889d149e210e0dcd8cded2aa433d7e
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2023-06-22 06:19:52 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ plugins {
     kotlin("jvm") version "1.8.22"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("com.diffplug.spotless") version "6.19.0"
+    id("org.cyclonedx.bom") version "1.7.4"
 }
 
 repositories {
